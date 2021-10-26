b02fdf85b2b76a8084f2024b3b99afeac35957a3
sivaganeshrk <sivadoog0808@gmail.com>
2021-10-26 18:03:36 +0530

    .gitignore

--- dist/utils/validator.d.ts
+++
@@ -1,6 +0,0 @@
-export declare class Validator {
-    static isNumber(val: any): boolean;
-    static isPositiveNumber(val: any): boolean;
-    static isObject(val: any): boolean;
-    static isNonEmptyObject(val: any): boolean;
-}
