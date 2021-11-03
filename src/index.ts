99d644783410bfd2121b182499aa5f673269ec5c
Siva Ganesh <sivadoog0808@gmail.com>
2021-11-03 13:30:51 +0530

    Updated export statement

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,5 @@
 import Transformer from "./core";
 import esb from "elastic-builder";
-export default Transformer;
 module.exports = Transformer;
 export const elasticBuilder = esb;
 export * from "./Types";
